ba29b9749231e006e50246262baeca92a8441294
MansoorAfzalDurrani <mansoorafzaldurrani@yahoo.com>
2023-11-06 18:30:36 +0000

    Both Assignments Copleted

--- exercise001.js
+++ exercise001.js
@@ -1,7 +1,6 @@
 const promise = new Promise((resolve, reject) => {
 	// Generate random number between 0 and 9
 	const randomInt = Math.floor(Math.random() * 10);
-
 	if (randomInt % 2 === 0) {
 		// Success
 		setTimeout(() => resolve('done'), 2000);
@@ -12,3 +11,10 @@ const promise = new Promise((resolve, reject) => {
 });
 
 // Your solution(s) to exercise001 here!
+promise
+.then((Response) => {
+	console.log('Yay! Promise resolved with response: done')
+})
+.catch((reject)=>{
+	console.log('Boo. Promise rejected with response. error')
+});
